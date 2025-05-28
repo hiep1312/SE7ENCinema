12793512c17e97c27f8f00b0d130097bd334af3b
Lê Danh Hiệp <ledanhhiep2005@gmail.com>
2025-05-28 10:50:45 +0700

    Update softDeletes, Library

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movie extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'title', 'description', 'duration', 'release_date', 'end_date',
         'director', 'actors', 'age_restriction', 'poster',
