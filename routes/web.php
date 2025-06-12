7ecb9b145185cfda7d10578fb0beb88336d29968
PhamHung02 <hungpsph49800@gmail.com>
2025-06-12 23:19:42 +0700

    sửa route và phần quản lý thể loại

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use App\Livewire\Admin\Genres\GenreCreate;
 use App\Livewire\Admin\Genres\GenreEdit;
 use App\Livewire\Admin\Genres\GenreIndex;
 use App\Livewire\Admin\Genres\GenreShow;
-use App\Livewire\Admin\Movies\MovieCreate;
-use App\Livewire\Admin\Movies\MovieEdit;
-use App\Livewire\Admin\Movies\MovieIndex;
-use App\Livewire\Admin\Movies\MovieShow;
-use App\Livewire\Admin\Movies\MovieTrash;
 
 
 
@@ -45,16 +40,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/{id}/edit', GenreEdit::class)->name('edit');
         Route::get('/{id}', GenreShow::class)->name('show');
     });
-
-    /* Movies */
-    Route::prefix('/movies')->name('movies.')->group(function () {
-        Route::get('/test', MovieTrash::class)->name('test');
-        Route::get('/', MovieIndex::class)->name('index');
-        Route::get('/create', MovieCreate::class)->name('create');
-        Route::get('/{movie}/edit', MovieEdit::class)->name('edit');
-        Route::get('/{movie}', MovieShow::class)->name('show');
-    });
-
 
     /* Template */
     Route::view('/dashboard', 'livewire.admin.template.dashboard')->name('dashboard');
