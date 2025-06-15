f61515c47e4225a330d216eeda133774c910b324
Lê Danh Hiệp <ledanhhiep2005@gmail.com>
2025-06-15 09:12:30 +0700

    Add wire ignore

--- resources/views/livewire/admin/ratings/rating-index.blade.php
+++ resources/views/livewire/admin/ratings/rating-index.blade.php
@@ -1,13 +1,13 @@
 <div>
     @if (session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert" wire:ignore.self>
+        <div class="alert alert-success alert-dismissible fade show" role="alert" wire:ignore>
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
     @endif
 
     @if (session()->has('error'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert" wire:ignore.self>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert" wire:ignore>
             {{ session('error') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
