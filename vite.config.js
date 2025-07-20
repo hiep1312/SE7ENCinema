277458b22b2ffdd9f579db7fb29c62029f99f768
Lê Danh Hiệp <ledanhhiep2005@gmail.com>
2025-07-20 19:11:58 +0700

    Update navigation

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/auth.css', 'resources/css/ticket.css', 'resources/css/scanner.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/auth.css', 'resources/css/ticket.css', 'resources/css/scanner.css', 'resources/js/scannerQR.js'],
             refresh: true,
         }),
     ],
