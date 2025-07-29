a794e46205c5130a572fc3ce6cad0cd96d26bc6a
quannguyen123 <nguyenquanhacker24072005@gmail.com>
2025-07-29 16:15:35 +0700

    abc

---
+++ resources/views/livewire/client/content-page.blade.php
@@ -0,0 +1,23 @@
+<div>
+    <!-- resources/views/livewire/client/content-page.blade.php -->
+<div class="product-heading">
+    <div class="con flex gap-2">
+        <select wire:model="selectedCategory" class="border rounded px-2 py-1">
+            <option value="">Tìm kiếm Phim</option>
+            <option value="movie">Phim hot</option>
+            <option value="video">Phim đang chiếu</option>
+            <option value="music">Phim Sắp chiếu</option>
+        </select>
+
+        <input wire:model.debounce.300ms="searchKeyword"
+            type="text" placeholder="Search Movie, Video, Music"
+            class="border rounded px-2 py-1 w-64" />
+
+        <button type="button" class="px-3 bg-gray-200 border rounded">
+            <i class="flaticon-tool"></i>
+        </button>
+    </div>
+</div>
+
+
+</div>
