758cc855d99994ff60d525d7be7b4526ff314407
Phạm Vũ Duy <duyvu1234g@gmail.com>
2025-08-06 23:39:02 +0700

    done notificationAdm

--- resources/views/livewire/admin/components/header.blade.php
+++ resources/views/livewire/admin/components/header.blade.php
@@ -101,51 +101,7 @@
         </div>
       </li>
       <li class="nav-item dropdown border-left">
-        <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
-          <i class="mdi mdi-bell"></i>
-          <span class="count bg-danger"></span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-          <h6 class="p-3 mb-0">Notifications</h6>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item preview-item">
-            <div class="preview-thumbnail">
-              <div class="preview-icon bg-dark rounded-circle">
-                <i class="mdi mdi-calendar text-success"></i>
-              </div>
-            </div>
-            <div class="preview-item-content">
-              <p class="preview-subject mb-1">Event today</p>
-              <p class="text-muted ellipsis mb-0"> Just a reminder that you have an event today </p>
-            </div>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item preview-item">
-            <div class="preview-thumbnail">
-              <div class="preview-icon bg-dark rounded-circle">
-                <i class="mdi mdi-settings text-danger"></i>
-              </div>
-            </div>
-            <div class="preview-item-content">
-              <p class="preview-subject mb-1">Settings</p>
-              <p class="text-muted ellipsis mb-0"> Update dashboard </p>
-            </div>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item preview-item">
-            <div class="preview-thumbnail">
-              <div class="preview-icon bg-dark rounded-circle">
-                <i class="mdi mdi-link-variant text-warning"></i>
-              </div>
-            </div>
-            <div class="preview-item-content">
-              <p class="preview-subject mb-1">Launch Admin</p>
-              <p class="text-muted ellipsis mb-0"> New admin wow! </p>
-            </div>
-          </a>
-          <div class="dropdown-divider"></div>
-          <p class="p-3 mb-0 text-center">See all notifications</p>
-        </div>
+        @livewire('admin.notification-admins.notification-adm')
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link" id="profileDropdown" href="#" data-toggle="dropdown">
@@ -175,8 +131,8 @@
                 <i class="mdi mdi-logout text-danger"></i>
               </div>
             </div>
-            <button type="submit" form="logout" class="preview-item-content" style="background: none; border: none;">
-              <p class="preview-subject mb-1">Đăng xuất</p>
+            <button type="submit" form="logout" class="preview-item-content" style="background: none; border: none; color: #fff;">
+              <p class="preview-subject mb-1 ">Đăng xuất</p>
             </button>
           </a>
           {{-- <div class="dropdown-divider"></div> --}}
