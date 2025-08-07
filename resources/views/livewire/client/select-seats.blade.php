3cbf8a455491d31e675c87722b7009153b24e31f
quannguyen123 <nguyenquanhacker24072005@gmail.com>
2025-08-08 00:47:03 +0700

    day thu len nhanh main

--- resources/views/livewire/client/select-seats.blade.php
+++ resources/views/livewire/client/select-seats.blade.php
@@ -4,7 +4,7 @@
 
 <div class="scRender" wire:poll.5000ms="refreshSeatStatus">
 
-<h1>test</h1>
+<h1>thử đẩy lên nhánh main</h1>
     @if ($isBanned && $banInfo)
         <div class="container mt-5">
             <div class="row justify-content-center mt-5">
