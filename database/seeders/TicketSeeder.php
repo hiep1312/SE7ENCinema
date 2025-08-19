0187e53b04cf7a6d880dde93a8363d74edb6a8f8
HongQuan <quanken2612@gmail.com>
2025-08-20 01:01:58 +0700

    ticket

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\BookingSeat;
 use App\Models\Ticket;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class TicketSeeder extends Seeder
 {
@@ -16,14 +16,17 @@ class TicketSeeder extends Seeder
     public function run(): void
     {
         $bookingSeatsId = BookingSeat::pluck('id')->toArray();
-        foreach (Arr::shuffle($bookingSeatsId) as $bookingSeatId) {
+
+        foreach ($bookingSeatsId as $bookingSeatId) {
+
+
             Ticket::create([
                 'booking_seat_id' => $bookingSeatId,
-                'note' => fake()->optional()->sentence(),
-                'qr_code' => fake()->uuid(),
-                'taken' => fake()->boolean(),
-                'status' => fake()->randomElement(['active', 'used', 'canceled']),
+                'note'   => fake()->optional()->sentence(),
+                'qr_code'=> fake()->uuid(),
+                'taken'  => fake()->boolean(),
+                'status' => Arr::random(['active', 'used', 'canceled']),
             ]);
-        };
+        }
     }
 }
