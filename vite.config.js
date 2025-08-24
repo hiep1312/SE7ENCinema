3a3668feae0ac9f4d698d58d1b287c84ab63d9ef
Lê Danh Hiệp <ledanhhiep2005@gmail.com>
2025-08-24 13:52:57 +0700

    Fix room

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 "resources/css/app.css",
-                'resources/css/NotificationIndex.css',
+                'resources/css/notificationIndex.css',
                 "resources/js/app.js",
                 "resources/css/auth.css",
                 "resources/css/ticket.css",
